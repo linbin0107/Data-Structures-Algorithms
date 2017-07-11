d3599496bc7617e7373ad78f85403b9b42099039
BIN LIN <linbinmr@gmail.com>
2017-07-10 22:26:21 -0700

    Implement several binary tree related algorithm, such as checking if a given binary tree is balanced, symmetric, tweaked identical, BST.

--- BinaryTree/src/Solution.java
+++ BinaryTree/src/Solution.java
@@ -1,4 +1,5 @@
 
+import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
@@ -112,6 +113,135 @@ public class Solution {
             } else {
                 curr = curr.right;
             }
+        }
+
+        return res;
+    }
+
+    /**
+     * Given a binary tree, determine if it's height balanced.
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null))
+            return true;
+
+        if (root.left == null && root.right != null &&
+                (root.right.left != null || root.right.right != null))
+            return false;
+
+        if (root.right == null && root.left != null &&
+                (root.left.left != null || root.left.right != null))
+            return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    /**
+     * Check if a given binary tree is symmetric
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null))
+            return true;
+
+        if ((root.left == null && root.right != null) ||
+                (root.left != null && root.right == null))
+            return false;
+
+        if (root.left.key != root.right.key)
+            return false;
+
+        return isIdentical(root.left.right, root.right.left) &&
+            isIdentical(root.left.left, root.right.right);
+    }
+
+    private boolean isIdentical(TreeNode one, TreeNode two) {
+        if (one == null && two == null)
+            return true;
+
+        if (one == null || two == null)
+            return false;
+
+        if (one.key != two.key)
+            return false;
+
+        return isIdentical(one.left, two.right) && isIdentical(one.right, two.left);
+    }
+
+    /**
+     * Determine whether two given binary trees are identical assuming any number of
+     * "tweak" is allowed. A tweak is defined as a swap of the children of a node in the tree.
+     * @param one
+     * @param two
+     * @return
+     */
+    public boolean isTweakedIdentical(TreeNode one, TreeNode two) {
+        if (one == null && two == null)
+            return true;
+
+        if (one == null && two != null)
+            return false;
+
+        if (one != null && two == null)
+            return false;
+
+        if (one.key != two.key)
+            return false;
+
+        return (isTweakedIdentical(one.left, two.left) && isTweakedIdentical(one.right, two.right)) ||
+                (isTweakedIdentical(one.right, two.left) && isTweakedIdentical(one.left, two.right));
+    }
+
+    /**
+     * Determine a given binary tree is a binary search tree
+     * @param root
+     * @return
+     */
+    public boolean isBST(TreeNode root) {
+        if (root == null)
+            return true;
+
+        return isValid(root.left, null, null);
+    }
+
+    private boolean isValid(TreeNode root, TreeNode minNode, TreeNode maxNode) {
+        if (root == null)
+            return true;
+
+        if ((minNode != null && minNode.key >= root.key) ||
+                maxNode != null && maxNode.key <= root.key)
+            return false;
+
+        return isValid(root.left, minNode, root) && isValid(root.right, root, maxNode);
+    }
+
+    /**
+     * Get the list of keys in a given binary search tree in a given range[min, max] in ascending order,
+     * both min and max are inclusive.
+     * @param root
+     * @param min
+     * @param max
+     * @return
+     */
+    public List<Integer> getRange(TreeNode root, int min, int max) {
+        List<Integer> res = new ArrayList<Integer>();
+
+        if (root == null)
+            return  res;
+
+        if (root.key < min)
+            res.addAll(getRange(root.right, min, max));
+
+        if (root.key > max)
+            res.addAll(getRange(root.left, min, max));
+
+        if (root.key >= min && root.key <= max) {
+            res.addAll(getRange(root.left, min, max));
+            res.add(root.key);
+            res.addAll(getRange(root.right, min, max));
         }
 
         return res;
